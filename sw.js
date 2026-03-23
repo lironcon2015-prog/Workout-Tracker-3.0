d4b17569f9001d3899eec88a65a12bf0bf4ead6b
lironcon2015-prog <lironcon2015@gmail.com>
2026-03-23 23:45:49 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
  * העלה את CACHE_VERSION בכל עדכון קוד כדי לרענן את ה-cache של המשתמשים.
  */
 
-const CACHE_VERSION = 'gympro-v14.11.0-6';
+const CACHE_VERSION = 'gympro-v14.11.0-7';
 
 const FILES_TO_CACHE = [
     './index.html',
